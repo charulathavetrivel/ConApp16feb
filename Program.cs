a17bfecd40691bd104a6dcb2c22b6bcae897b84f
Charulatha <charulatha200701@gmail.com>
2024-02-16 09:33:34 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp16feb;
+
+Console.WriteLine("Welcome to number system");
+Console.WriteLine("--------Even Number -------");
+NumberSystem.EvenNum();
+Console.WriteLine("--------Odd Number -------");
+NumberSystem.OddNum();
